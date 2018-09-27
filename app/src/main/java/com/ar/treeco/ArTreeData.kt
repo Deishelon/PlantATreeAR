e9c92bfe5aa996fac29b3d1916f7f2410f8c4bc0
deishelon <deishelon@gmail.com>
2018-09-28 11:57:23 +1200

    Added more tree

--- app/src/main/java/com/ar/treeco/ArTreeData.kt
+++ app/src/main/java/com/ar/treeco/ArTreeData.kt
@@ -7,6 +7,10 @@ fun getArTreeData(): ArrayList<ArTree> {
 
     arTreeList.add(ArTree("Coffee tree", R.drawable.ar_preview_coffe, R.raw.tree))
     arTreeList.add(ArTree("Palm tree", R.drawable.ar_preview_palm, R.raw.palm_tree))
+    arTreeList.add(ArTree("Pine tree", R.drawable.ar_preview_pine_tree, R.raw.pine_tree))
+    arTreeList.add(ArTree("Banana tree", R.drawable.ar_preview_banana, R.raw.banana_tree))
+    arTreeList.add(ArTree("Ivory cane palm tree", R.drawable.ar_preview_ivory, R.raw.ivory_tree))
+    arTreeList.add(ArTree("Sakura tree", R.drawable.ar_preview_sakura, R.raw.sakura_tree))
 
     return arTreeList
 }
